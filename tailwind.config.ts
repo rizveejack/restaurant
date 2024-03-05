b5da5f66f32113e1f0e0d5adabdb007cd1a426fd
rizveejack <jrizvee@gmail.com>
2024-03-05 13:13:36 +0600

    inetial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/utils/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/partials/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {},
